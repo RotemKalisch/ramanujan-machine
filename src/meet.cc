162102222d3732ab4e0c2bd6a1ac1c4632575856
Rotem Kalisch <rotemkalisch@gmail.com>
2022-02-15 19:03:26 +0200

    Change parameters

--- src/meet.cc
+++ src/meet.cc
@@ -5,20 +5,22 @@
 
 namespace ramanujan {
 
-std::vector<rhs::number_generator_t> create_linear_functions(int32_t thresh) {
+std::vector<rhs::number_generator_t> create_linear_functions(int32_t threshold) {
     std::vector<rhs::number_generator_t> results;
-    if (thresh < 0) return results;
-    results.push_back(rhs::make_polynomial({4, 1}));
-    results.push_back(rhs::make_polynomial({0, -1}));
+    for (int32_t m = -threshold; m <= threshold; ++m) {
+        for (int32_t n = - threshold; n <= threshold; ++n) {
+            results.push_back(rhs::make_polynomial({n, m}));
+        }
+    }
     return results;
 }
  
 std::vector<rhs::number_generator_t> create_generators() {
-    return create_linear_functions(20);
+    return create_linear_functions(/*threshold=*/20);
 }
 
 void load(MeetMap& meet_map) {
-    lhs::enumerate(meet_map, 10, const_pairs);
+    lhs::enumerate(meet_map, /*threshold=*/10, const_pairs);
 }
 
 void meet(
@@ -26,7 +28,7 @@ void meet(
     const std::vector<rhs::number_generator_t>& a_gens,
     const std::vector<rhs::number_generator_t>& b_gens
 ) {
-    enumerate(meet_map, a_gens, b_gens, 10000);
+    enumerate(meet_map, a_gens, b_gens, /*depth=*/100);
 }
 
 } // namespace ramanujan
