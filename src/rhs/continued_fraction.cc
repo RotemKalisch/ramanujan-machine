def652ce3e6b0163e7f970ba73f8d010d7278bed
Rotem Kalisch <rotemkalisch@gmail.com>
2022-02-15 13:25:03 +0200

    Split continued_fraction to cc file

---
+++ src/rhs/continued_fraction.cc
@@ -0,0 +1,28 @@
+#include "src/rhs/continued_fraction.h"
+
+#include <algorithm>
+#include <vector>
+
+namespace ramanujan::rhs {
+
+double calculate_continued_fraction(
+    const NumberGenerator& a, const NumberGenerator& b, size_t depth
+) {
+    a.reset();
+    b.reset();
+    double result = 0;
+    std::vector<coef_t> a_results, b_results;
+    a_results.reserve(depth);
+    b_results.reserve(depth);
+    for (size_t i = 0; i <= std::max(1UL, depth); ++i) {
+        a_results.push_back(a());
+        b_results.push_back(b());
+    }
+    for (size_t i = depth; i > 0; --i) {
+        result = b_results[i] / (a_results[i] + result);
+    }
+    result += a_results[0];
+    return result;
+}
+
+} // namespace ramanujan::rhs
