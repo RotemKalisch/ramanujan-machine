96587b48a4aa4e8896e0d61efa58de5ae0ef9533
Rotem Kalisch <rotemkalisch@gmail.com>
2022-02-17 16:30:57 +0200

    Update LHS enumerator performance test to also print rate

--- test/lhs/enumerator_performance_test.cc
+++ test/lhs/enumerator_performance_test.cc
@@ -22,10 +22,12 @@ TEST(Enumerator, performance) {
     const auto start = std::chrono::steady_clock::now();
     enumerate<>(test_map, 40, const_pair);
     const auto end = std::chrono::steady_clock::now();
-    const double unique_generated_bits = std::log2(test_map.size());
-    std::cout << "Generated " << unique_generated_bits << " bit LHS results in " << 
-        std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() <<
-        " milliseconds." << std::endl;
+    const double unique_generated = test_map.size();
+    const auto time_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    const auto time_s = std::chrono::duration_cast<std::chrono::seconds>(end - start);
+    std::cout << "Generated " << std::log2(unique_generated) << " bit LHS results in " << 
+        time_ms.count() << " milliseconds." << std::endl <<
+         "Rate: " << std::log2(unique_generated / time_s.count()) << " bits/s" << std::endl;
 }
 
 } // namespace ramanujan
